c9a38e3bda069363fa0e4d07ea54afa419c50530
Paulo Miguel <paulo2.0miguel@gmail.com>
2023-09-08 14:34:59 -0300

    commit inicial

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDGuIqRzbYFHvglv--PnZZbFyKmad_B5lg",
+  authDomain: "colegio-f65ee.firebaseapp.com",
+  projectId: "colegio-f65ee",
+  storageBucket: "colegio-f65ee.appspot.com",
+  messagingSenderId: "901245399761",
+  appId: "1:901245399761:web:870c80f1f9de20acc4cbf0"
+};  
+
+firebase.initializeApp(firebaseConfig)
+firebase.firestore().settings({
+        experimentalForceLongPolling: false,
+        merge: true,
+      });
+const db = firebase.firestore()
